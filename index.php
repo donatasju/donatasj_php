cefa6ba1d27835b0f56aa05b2d94dfab9228eca8
donatasju <donatas.jurkusl@gmail.com>
2018-10-16 12:54:25 +0300

    Trecias commit

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
-<?php echo 'Veikia su GIT' ?>
-<html>
-    <body>
-        <h1>
-            Nieko nenoriu sakyt
-        </h1>
-    </body
-    
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>As valdau HTML !</title>
+  </head>
+  <body>
+      <h1>
+          Donatas - HTML asas !
+      </h1>
+      <p>
+          Moku kurti statini turini.
+      </p>
+  </body>
 </html>
