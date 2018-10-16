3a0ee23d45046b197290e82e2bf00582c7784531
donatasju <donatas.jurkusl@gmail.com>
2018-10-16 15:06:55 +0300

    treeecias

--- index.php
+++ index.php
@@ -3,16 +3,21 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>PHP lydes ir <?php date_default_timezone_set('Europe/Vilnius') .  print date('Y.m.d', strtotime('+1 day')) ?></title>
+    <title>PHP lydes ir <?php date_default_timezone_set('Europe/Vilnius') . print date('Y', rand(strtotime('+1 year'), strtotime('+10 year'))) ?></title>
   </head>
   <body>
       <h1>
-          <i>Donatas</i> - HTML <?php print date('H', strtotime('-1 hour')) . ' valanda!'?>
+          <i>Donatas</i> - Galbut turesiu <?php print rand(1,5) . ' vaiku(us)'?>
       </h1>
       <p>
-          <?php print date('Y', strtotime('+1 year'))?> ne uz kalnu!
+          
+          D. Trump'as nebebus presidentu <?php print date('Y.m.d', strtotime('+' . rand(3, 10)  . ' years'))?>
       </p>
   </body>
 </html>
 
+
+<?php // sets default timezone ?>
 <?php // date_default_timezone_set('Europe/Vilnius'); print 'Data:' . date("Y.m.d H.i.s")?>
+
+   <!--<title>PHP lydes ir <?php date_default_timezone_set('Europe/Vilnius') .  print date('Y.m.d', strtotime('+1 day')) ?></title>-->
