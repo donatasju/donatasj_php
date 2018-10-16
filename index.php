962b14440acc2fba1163ed5431fdef66e608cbe6
donatasju <donatas.jurkusl@gmail.com>
2018-10-16 15:39:24 +0300

    ketvirtas

--- index.php
+++ index.php
@@ -5,13 +5,11 @@
     <meta charset="utf-8">
     <title>PHP lydes ir <?php date_default_timezone_set('Europe/Vilnius') . print date('Y', rand(strtotime('+1 year'), strtotime('+10 year'))) ?></title>
   </head>
-  <body>
-      <h1>
+  <body style="background-color: rgb(<?php print rand(0, 255) . ',' . rand(0, 255) . ',' . rand(0, 255)?>);">
+      <h1 style="font-size: <?php print rand(3, 30) . 'px'?>">
           <i>Donatas</i> - Galbut turesiu <?php print rand(1,5) . ' vaiku(us)'?>
       </h1>
-      <p>
-          
-          D. Trump'as nebebus presidentu <?php print date('Y.m.d', strtotime('+' . rand(3, 10)  . ' years'))?>
+      <p style="color: rgb(<?php print rand(0, 255) . ',' . rand(0, 255) . ',' . rand(0, 255)?>)">D. Trump'as nebebus presidentu <?php print date('Y.m.d', strtotime('+' . rand(3, 10)  . ' years'))?>
       </p>
   </body>
 </html>
