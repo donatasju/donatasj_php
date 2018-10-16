04b1c6bc8602da1a81e4d673ebefe664b51d2731
donatasju <donatas.jurkusl@gmail.com>
2018-10-16 14:12:00 +0300

    anttras

--- index.php
+++ index.php
@@ -3,15 +3,16 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title><?php print 'Aš,' . date('l') . ' ir PHP' ?></title>
+    <title>PHP lydes ir <?php date_default_timezone_set('Europe/Vilnius') .  print date('Y.m.d', strtotime('+1 day')) ?></title>
   </head>
   <body>
       <h1>
-          <i>Donatas</i> - HTML <?php print "ir PHP"?> asas jau nuo <?php print date('Y') . ' metu' ?>
+          <i>Donatas</i> - HTML <?php print date('H', strtotime('-1 hour')) . ' valanda!'?>
       </h1>
       <p>
-          Viskas prasidejo <?php print date('m') . ' menesio,' . date(' d') . ' diena!'?>
+          <?php print date('Y', strtotime('+1 year'))?> ne uz kalnu!
       </p>
   </body>
 </html>
 
+<?php // date_default_timezone_set('Europe/Vilnius'); print 'Data:' . date("Y.m.d H.i.s")?>
