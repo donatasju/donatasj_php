39a56f083858fb61638adddac80163639a11b857
donatasju <donatas.jurkusl@gmail.com>
2018-10-16 13:44:33 +0300

    dadeta data

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>As valdau HTML !</title>
+    <title>As valdau HTML! <?php print "ir PHP" ?></title>
   </head>
   <body>
       <h1>
-          Donatas - HTML asas !
+          <i>Donatas</i> - HTML <?php print "ir PHP"?> asas !
       </h1>
       <p>
-          Moku kurti statini turini.
+          Moku kurti statini turini. Atidaryta: <?php date_default_timezone_set('Europe/Vilnius'); print date("Y.m.d H.i.s")?>
       </p>
   </body>
 </html>
+
