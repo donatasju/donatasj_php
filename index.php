459e150a3027d758870f5d21e504745ae764f180
donatasju <donatas.jurkusl@gmail.com>
2018-10-17 12:43:08 +0300

    Variables

--- index.php
+++ index.php
@@ -1,21 +1,7 @@
-
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>PHP lydes ir <?php date_default_timezone_set('Europe/Vilnius') . print date('Y', rand(strtotime('+1 year'), strtotime('+10 year'))) ?></title>
-  </head>
-  <body style="background-color: rgb(<?php print rand(0, 255) . ',' . rand(0, 255) . ',' . rand(0, 255)?>);">
-      <h1 style="font-size: <?php print rand(3, 30) . 'px'?>">
-          <i>Donatas</i> - Galbut turesiu <?php print rand(1,5) . ' vaiku(us)'?>
-      </h1>
-      <p style="color: rgb(<?php print rand(0, 255) . ',' . rand(0, 255) . ',' . rand(0, 255)?>)">D. Trump'as nebebus presidentu <?php print date('Y.m.d', strtotime('+' . rand(3, 10)  . ' years'))?>
-      </p>
-  </body>
-</html>
-
-
-<?php // sets default timezone ?>
-<?php // date_default_timezone_set('Europe/Vilnius'); print 'Data:' . date("Y.m.d H.i.s")?>
-
-   <!--<title>PHP lydes ir <?php date_default_timezone_set('Europe/Vilnius') .  print date('Y.m.d', strtotime('+1 day')) ?></title>-->
+<?php $string = 'stringas ' ;
+      $integer = 123  ;
+      $float =  123.123 ;
+      $boolean = True ;
+      $null = null ; 
+      print $string . ' ' . $integer . ' ' . $float . ' ' . $boolean . ' ' . $null ;
+      ?>
