7a50bd6042da948b282c43c1cdf5eca9c454c49c
donatasju <donatas.jurkusl@gmail.com>
2018-10-19 15:08:05 +0300

    Array1

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
-<?php 
+<?php
 $fridge = ['Jogurtas, ', 'Kebabas, ', 'alus, ', 'sugede vaisiai, ', 'supuves avokadas. '];
 
 $text = ' ';
-for ($i = 0; $i < count($fridge); $i++) {
-    $text .= $fridge[$i] ;
+foreach ($fridge as $produktas) {
+    $text .= $produktas;
 }
 ?>
 <html>
     <body>
         <h1><?php print $text; ?></h1>
+        <h1><?php print $fridge [rand(0, count($fridge) - 1)]; ?></h1>
     </body>
 </html>
