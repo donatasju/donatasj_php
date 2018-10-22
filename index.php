bac6dd52080887697f75534e3f37975718f1d8bc
donatasju <donatas.jurkusl@gmail.com>
2018-10-22 11:47:26 +0300

    Array1

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
 <?php
-$fridge = ['Jogurtas, ', 'Kebabas, ', 'alus, ', 'sugede vaisiai, ', 'supuves avokadas. '];
-
+$fridge = ['Jogurtas, ', 'Kebabas', 'alus, ', 'sugede vaisiai, ', 'supuves avokadas. '];
+$noriu = ['Kebabas', 'alus, ', 'Pica'];
 $text = ' ';
-foreach ($fridge as $produktas) {
-    $text .= $produktas;
+
+
+foreach ($fridge as $key => $produktas) {
+    if (in_array ($produktas, $noriu)) {
+        $text .= "$produktas: Isemiau <br>";
+        unset ($fridge['Kebabas']);
+    } else {
+        $text .= "$produktas: Palikau <br>";
+    }
 }
 ?>
+
 <html>
     <body>
-        <h1><?php print $text; ?></h1>
-        <h1><?php print $fridge [rand(0, count($fridge) - 1)]; ?></h1>
+        <h1>
+            <?php print $text; ?>
+        </h1>
     </body>
 </html>
